a9ba34b81871fb9413c8b34bbdf06b4bd60beb89
lenka-w1 <lenka-w1@mail.ru>
2021-03-07 03:31:21 +0300

    feat: add function solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,46 @@
 
 exports.min = function min (array) {
-  return 0;
+  let minDigit = 0;
+  if (array == undefined) {
+    return 0;
+  } else if (array.length < 1) {
+    return 0;
+  } else {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] < minDigit) {
+        minDigit = array[i];
+      }
+    }
+    return minDigit;
+  }
 }
 
 exports.max = function max (array) {
-  return 0;
+  let maxDigit = 0;
+  if (array == undefined) {
+    return 0;
+  } else if (array.length < 1){
+    return 0;
+  } else {
+    for (let i = 0; i < array.length; i++) {
+      if (array[i] > maxDigit) {
+        maxDigit = array[i];
+      }
+    }
+    return maxDigit;
+  }
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  let sum = 0;
+  if (array == undefined) {
+    return 0;
+  } else if (array.length < 1) {
+    return 0;
+  } else {
+    for (let i = 0; i < array.length; i++) {
+      sum += array[i];
+    }
+    return sum/array.length;
+  }
 }
